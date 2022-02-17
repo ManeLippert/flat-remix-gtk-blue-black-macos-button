003c5eeaac1e765e4c6648128d5845535e005657
ManeLippert <manuel-lippert@outlook.de>
2022-02-17 21:28:35 +0100

    Rename Folders

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-sudo rm -rf /usr/share/themes/Flat-Remix-GTK4-Blue-Black-Solid-NoBorder-MacOS-Button
-sudo cp -r Flat-Remix-GTK4-Blue-Black-Solid-NoBorder-MacOS-Button /usr/share/themes/Flat-Remix-GTK4-Blue-Black-Solid-NoBorder-MacOS-Button
+sudo rm -rf /usr/share/themes/Flat-Remix-GTK4-Blue-Darkest-Solid-NoBorder-MacOS-Button
+sudo cp -r Flat-Remix-GTK4-Blue-Darkest-Solid-NoBorder-MacOS-Button /usr/share/themes/Flat-Remix-GTK4-Blue-Darkest-Solid-NoBorder-MacOS-Button
