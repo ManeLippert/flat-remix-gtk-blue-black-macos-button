6aecc7f84fdf5dbe3366bb533007b72ba8981643
ManeLippert <manuel-lippert@outlook.de>
2022-02-17 18:46:57 +0100

    Update installer

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-sudo cp Flat-Remix-GTK4-Blue-Black-Solid-NoBorder-MacOS-Button /usr/share/themes/Flat-Remix-GTK4-Blue-Black-Solid-NoBorder-MacOS-Button
+sudo cp -r Flat-Remix-GTK4-Blue-Black-Solid-NoBorder-MacOS-Button /usr/share/themes/Flat-Remix-GTK4-Blue-Black-Solid-NoBorder-MacOS-Button
